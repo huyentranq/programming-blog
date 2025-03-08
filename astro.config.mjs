203693e6c11fd363aaa87893e688a3f519b05f55
HuyenTrang457 <nguyenhuyentrangg457@gmail.com>
2025-03-08 20:47:56 +0700

    m

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
+
+export default defineConfig({
+  site: 'https://your-blog-domain.com',
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+  ],
+  output: 'static',
+  adapter: netlify(),
+});
